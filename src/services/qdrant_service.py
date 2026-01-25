913aa9fd5ae5882e9f2ed12fcb1bb03327545329
Nishant Achwan <achwannishant2003@gmail.com>
2026-01-26 00:13:05 +0530

    added some functionalities

---
+++ src/services/qdrant_service.py
@@ -0,0 +1,50 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams, PointStruct
+
+
+
+class QdrantService:
+
+    def __init__(self, path: str = "./qdrant_data"):
+        self.client = QdrantClient(path=path)
+
+    def ensure_collection(self, collection_name: str, vector_size: int) -> None:
+    #create collection if not exists
+
+        existing = [c.name for c in self.client.get_collections().collections]
+
+        if collection_name in existing:
+            return
+
+        self.client.create_collection(
+            collection_name=collection_name,
+            vectors_config=VectorParams(
+                size=vector_size,
+                distance=Distance.COSINE
+            )
+        )
+
+    def add_document(self,collection_name: str,doc_id: int, vector: list[float], payload: dict) -> None:
+   
+
+        point = PointStruct(
+            id=doc_id,
+            vector=vector,
+            payload=payload
+        )
+        self.client.upsert(
+            collection_name=collection_name,
+            points=[point]
+        )
+
+
+    def search_documents(self, collection_name: str, query_vector: list[float], limit: int = 5):
+        
+        results = self.client.query_points(
+            collection_name=collection_name,
+            query=query_vector,
+            limit=limit,
+        with_payload=True
+    )
+
+        return results.points
